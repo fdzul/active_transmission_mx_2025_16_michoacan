4fb7450efe7dcf188e6c813f3efec79d7294be9c
fdzul <felipe.dzul.m@gmail.com>
2025-06-02 14:50:50 -0600

    This commit update the dash

--- mapas_de_calor.R
+++ mapas_de_calor.R
@@ -2,13 +2,27 @@
 # Step 1. load the dataset ####
 load("/Users/felipedzul/Library/CloudStorage/OneDrive-Personal/proyects/geocoding_mex/2025/8.RData/denmex_2025_edo16.RData")
 
+load("/Users/felipedzul/OneDrive/proyects/geocoding_mex/2025/8.RData/denmex_2025.RData")
+
 
 # Step 2. hacer el mapa de calor morelia ####
-densnv::mp_heatmap(geocoded_datasets = z_mich,
+densnv::mp_heatmap(geocoded_datasets = z,
+                   cve_edo = "16",
+                   locality = "Uruapan",
+                   status_caso = c(1, 2),
+                   week = c(17:22),
+                   zoom = NULL,
+                   map_type = NULL,
+                   alpha = 0.6,
+                   static = FALSE,
+                   palette = viridis::turbo)
+
+
+densnv::mp_heatmap(geocoded_datasets = z,
                    cve_edo = "16",
                    locality = "Morelia",
                    status_caso = c(1, 2),
-                   week = c(1:53),
+                   week = c(17:22),
                    zoom = NULL,
                    map_type = NULL,
                    alpha = 0.6,
